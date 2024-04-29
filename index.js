6fbc784f5a35dac2194c0dda01143696b8643d3a
Olena <ivanova.olenk@gmail.com>
2024-04-29 11:46:26 -0700

    add .gitignore, fix hw according to the comments

--- index.js
+++ index.js
@@ -20,22 +20,18 @@ async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       const allContacts = await listContacts();
-      return console.log(allContacts);
-      break;
+      return console.table(allContacts);
 
     case "get":
       const contactById = await getContactById(id);
       return console.log(contactById);
-      break;
-
     case "add":
       const newContact = await addContact(name, email, phone);
       return console.log(newContact);
-      break;
 
     case "remove":
       const deleteContact = await removeContact(id);
-      break;
+      return console.log(deleteContact ? deleteContact : null);
 
     default:
       console.warn("\x1B[31m Unknown action type!");
